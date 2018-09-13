b6e56e8329f989a6281b42ad267abb875635026d
jtaylorcpp <jtaylorcpp@gmail.com>
2018-09-13 18:23:18 -0400

    simple reverse proxyt example before moving forward

--- cmd/simple/main.go
+++ cmd/simple/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"os"
 	proxy "github.com/jtaylorcpp/gammacorp-proxy"
 )
 
 func main() {
-	proxy.NewSimpleReverseProxy("http://defaultbackend:8080")
+	remote,ok := os.LookupEnv("SIMPLEBACKEND")
+	if !ok {
+		remote = "http://default-backend:8080"
+	}
+	proxy.NewSimpleReverseProxy(remote)
 }
--- reverseproxy.go
+++ reverseproxy.go
@@ -1,11 +1,24 @@
 package proxy
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+
 	"github.com/gorilla/mux"
+	zipkin "github.com/openzipkin/zipkin-go"
+	middleware "github.com/openzipkin/zipkin-go/middleware/http"
+	reporter "github.com/openzipkin/zipkin-go/reporter/http"
 )
+
+type ReverseProxy struct {
+	target *url.URL
+	proxy  *httputil.ReverseProxy
+	tracer *zipkin.Tracer
+}
 
 func NewSimpleReverseProxy(target string) {
 	remote, err := url.Parse(target)
@@ -13,17 +26,63 @@ func NewSimpleReverseProxy(target string) {
 		panic(err)
 	}
 
-	rproxy := httputil.NewSingleHostReverseProxy(remote)
+	var zipkinAddr string
+	zipkinAddr, ok := os.LookupEnv("ZIPKIN")
+	if !ok {
+		zipkinAddr = "http://zipkin:9411/api/v2/spans"
+	}
+
+	fmt.Println("ataching to zipkin at: ", zipkinAddr)
+
+	zipkinReporter := reporter.NewReporter(zipkinAddr)
+	zipkingEndpoint, err := zipkin.NewEndpoint(
+		"simple-reverseproxy",
+		"127.0.0.1:8080")
+	if err != nil {
+		panic(err)
+	}
+
+	ZipkinSampler, err := zipkin.NewCountingSampler(1)
+	if err != nil {
+		panic(err)
+	}
+
+	tracer, err := zipkin.NewTracer(
+		zipkinReporter,
+		zipkin.WithSampler(ZipkinSampler),
+		zipkin.WithLocalEndpoint(zipkingEndpoint),
+		zipkin.WithSharedSpans(true),
+		zipkin.WithTraceID128Bit(true),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	zipkinMiddleware := middleware.NewServerMiddleware(
+		tracer,
+		middleware.SpanName("simple-reverseproxy"),
+		middleware.TagResponseSize(true),
+	)
+
+	rp := &ReverseProxy{
+		target: remote,
+		proxy:  httputil.NewSingleHostReverseProxy(remote),
+		tracer: tracer,
+	}
+
+	rpTransport, err := middleware.NewTransport(
+		tracer,
+		middleware.TransportTrace(true),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	rp.proxy.Transport = rpTransport
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", simplehandler(rproxy))
+	r.HandleFunc("/", rp.proxy.ServeHTTP)
+	r.Use(zipkinMiddleware)
 
-	http.Handle("/", r)
-	http.ListenAndServe(":8080", r)
-}
-
-func simplehandler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		p.ServeHTTP(w, r)
-	}
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
